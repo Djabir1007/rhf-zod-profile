f7e4741a5562b6a480117d77b093bcca9bbfb66e
Djabir1007 <jabir.pashaev@gmail.com>
2026-06-27 00:23:00 +0300

    сверстать форму профиля

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,9 +1,13 @@
+import { ProfileForm } from "@/features/profile-form/ui/ProfileForm/ProfileForm";
 import styles from "./App.module.css";
 
 function App() {
   return (
     <div className={styles.container}>
       <h1 className={styles.title}>Форма профиля</h1>
+      <main>
+        <ProfileForm />
+      </main>
     </div>
   );
 }
