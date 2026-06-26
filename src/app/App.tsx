bd9e0c258f19fc2ad72f9287b714b69aeb93d309
Djabir1007 <jabir.pashaev@gmail.com>
2026-06-26 18:41:48 +0300

    инициализировать структуру проекта

---
+++ src/app/App.tsx
@@ -0,0 +1,11 @@
+import styles from "./App.module.css";
+
+function App() {
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>Форма профиля</h1>
+    </div>
+  );
+}
+
+export default App;
